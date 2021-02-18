a64b93287369094872b605764f0d9150bccd8b8e
GROUNDBATTLES <77581209+groundbattles@users.noreply.github.com>
2021-02-18 15:29:52 +0100

    Update ts3audiobotinstaller.sh

--- ts3audiobotinstaller.sh
+++ ts3audiobotinstaller.sh
@@ -1,7 +1,7 @@
 echo "Downloade Sachen..."
 apt-get install sudo -y
 apt-get install gpg -y
-wget wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
 mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
 wget -q https://packages.microsoft.com/config/debian/9/prod.list
 mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
